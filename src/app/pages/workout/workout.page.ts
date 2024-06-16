550afe203a783747b1284a15b5fa666e27a1f5e5
Ramaraj T <jsramraj@gmail.com>
2024-06-16 16:08:33 +0530

    add filter option to view workouts based on muscle group

--- src/app/pages/workout/workout.page.ts
+++ src/app/pages/workout/workout.page.ts
@@ -4,11 +4,19 @@ import {
   IonToolbar,
   IonTitle,
   IonContent,
+  IonItem,
+  IonLabel,
+  IonSelectOption,
+  IonSelect,
+  SelectChangeEventDetail,
 } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../../components/explore-container/explore-container.component';
 import { ExcercisesService } from 'src/services/excercises.service';
 import Excercise from 'src/models/Excercise';
 import { WorkoutListComponent } from '../../components/workout-list/workout-list.component';
+import { ChangeDetectorRef } from '@angular/core';
+import { IonSelectCustomEvent } from '@ionic/core';
+import Exercise from 'src/models/Excercise';
 
 @Component({
   selector: 'app-workout',
@@ -16,6 +24,10 @@ import { WorkoutListComponent } from '../../components/workout-list/workout-list
   styleUrls: ['workout.page.scss'],
   standalone: true,
   imports: [
+    IonLabel,
+    IonSelect,
+    IonSelectOption,
+    IonItem,
     IonHeader,
     IonToolbar,
     IonTitle,
@@ -26,10 +38,84 @@ import { WorkoutListComponent } from '../../components/workout-list/workout-list
 })
 export class Tab1Page {
   workouts: Excercise[] = [];
-  constructor(private excerciseService: ExcercisesService) {}
+  majorGroupsBasic: { [key: string]: string[] } = {
+    all: [
+      'abdominals',
+      'hamstrings',
+      'adductors',
+      'quadriceps',
+      'calves',
+      'glutes',
+      'abductors',
+      'biceps',
+      'triceps',
+      'forearms',
+      'shoulders',
+      'traps',
+      'neck',
+      'middle back',
+      'lower back',
+      'lats',
+      'chest',
+    ],
+    core: ['abdominals'],
+    legs: [
+      'hamstrings',
+      'adductors',
+      'quadriceps',
+      'calves',
+      'glutes',
+      'abductors',
+    ],
+    arms: ['biceps', 'triceps', 'forearms'],
+    'upper body': ['shoulders', 'traps', 'neck'],
+    back: ['middle back', 'lower back', 'lats'],
+    chest: ['chest'],
+  };
+
+  muscleGroupsAdvanced: string[] = [
+    'abdominals',
+    'hamstrings',
+    'adductors',
+    'quadriceps',
+    'biceps',
+    'shoulders',
+    'chest',
+    'middle back',
+    'calves',
+    'glutes',
+    'lower back',
+    'lats',
+    'triceps',
+    'traps',
+    'forearms',
+    'neck',
+    'abductors',
+  ];
+  majorGroupsNames: string[] = [];
+  filteredWorkouts: Exercise[] = [];
+  constructor(
+    private excerciseService: ExcercisesService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
     this.workouts = this.excerciseService.getAllExcercises();
-    console.log(this.workouts[0]);
+    this.filteredWorkouts = this.workouts;
+    this.majorGroupsNames = Object.keys(this.majorGroupsBasic);
+    console.log(this.filteredWorkouts[0]);
+    this.cdr.detectChanges();
+  }
+
+  handleChange($event: IonSelectCustomEvent<SelectChangeEventDetail<any>>) {
+    const muscleGroup = $event.detail.value;
+    if (muscleGroup === 'all') {
+      this.filteredWorkouts = this.workouts;
+    } else {
+      this.filteredWorkouts = this.workouts.filter((workout) =>
+        this.majorGroupsBasic[muscleGroup].includes(workout.primaryMuscles[0])
+      );
+    }
+    this.cdr.detectChanges();
   }
 }
