577efbd3a83450e53e0ee2596f3a4ab164d6bbec
Ramaraj T <jsramraj@gmail.com>
2025-01-15 15:07:09 +0530

    update the app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,11 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'workout-tracker',
+  appName: 'workout-planner',
   webDir: 'www',
   server: {
-    androidScheme: 'https'
-  }
+    androidScheme: 'https',
+  },
 };
 
 export default config;
