653e9cfb6f64824ad7d940fbe7038a8da554e8d1
Ramaraj T <jsramraj@gmail.com>
2025-01-19 19:36:17 +0530

    update the download url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  workoutPlanUrl : 'https://drive.google.com/uc?export=download&id=1Rop2mco0XsLVMx_U3-iGjZj9ne0naOTl'
+  workoutPlanUrl : 'https://drive.usercontent.google.com/download?id=1Rop2mco0XsLVMx_U3-iGjZj9ne0naOTl&export=download'
 };
