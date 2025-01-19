2861ccfad1ea929dffb89d28b915b0068f256f1a
Ramaraj T <jsramraj@gmail.com>
2025-01-19 20:03:06 +0530

    update the planner json url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  workoutPlanUrl : 'https://drive.usercontent.google.com/download?id=1Rop2mco0XsLVMx_U3-iGjZj9ne0naOTl&export=download'
+  workoutPlanUrl : 'https://musclemadness.github.io/datasource/planner.json'
 };
