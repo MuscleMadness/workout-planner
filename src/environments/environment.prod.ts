4872fdf4773c0b9732d6b6dcafa6f854b4dc5d79
Ramaraj T <jsramraj@gmail.com>
2025-03-30 07:18:23 +0530

    remove api key from code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
   production: true,
   workoutPlanBaseUrl : 'https://musclemadness.co.in/datasource/',
-  defaultWorkoutPlanUrl : '../../assets/data/default-workout-plan.json',
-  googleDriveApiKey : 'AIzaSyBnG126MjnTc_Ro8yAZQAxTMdpWrIlPIRM'
+  defaultWorkoutPlanUrl : '../../assets/data/default-workout-plan.json'
 };
