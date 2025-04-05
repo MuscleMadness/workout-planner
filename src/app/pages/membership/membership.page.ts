33f5aef5db4569cbf203cb47f406cc85c40a0d3a
Ramaraj T <jsramraj@gmail.com>
2025-04-05 13:17:51 +0530

    add sigin with google page

--- src/app/pages/membership/membership.page.ts
+++ src/app/pages/membership/membership.page.ts
@@ -1,8 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Platform, ToastController } from '@ionic/angular';
 import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
 import { Gym, User } from 'src/app/models/gym';
 import { GymManagementService } from 'src/app/services/gym-management.service';
+import { environment } from 'src/environments/environment';
+
+import {
+  GoogleLoginResponse,
+  GoogleLoginResponseOnline,
+  SocialLogin,
+} from '@capgo/capacitor-social-login';
 
 @Component({
   selector: 'app-membership',
@@ -18,13 +26,28 @@ export class MembershipPage implements OnInit {
   phoneNumber: string = '';
   userInfo: User | null = null;
   showRegisterButton = false;
+  isLoggedIn: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
+    private iab: InAppBrowser,
+    private platform: Platform,
     private gymManagementService: GymManagementService,
-    private iab: InAppBrowser
-  ) {}
+    private toastCtrl: ToastController
+  ) {
+    this.initializeApp();
+  }
+
+  initializeApp() {
+    this.platform.ready().then(async () => {
+      await SocialLogin.initialize({
+        google: {
+          webClientId: environment.googleSignInClientId, // the web client id for Android and Web
+        },
+      });
+    });
+  }
 
   ngOnInit() {
     // Read gymId from query params
@@ -38,12 +61,76 @@ export class MembershipPage implements OnInit {
     this.showCurrentUrl();
   }
 
+  ionViewWillEnter() {
+    this.loadSessionDetails();
+  }
+
+  loadSessionDetails() {
+    // Check if user is logged in
+    const authToken = localStorage.getItem('authToken');
+    if (authToken) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
+  }
+
   currentUrl: string = '';
   async showCurrentUrl() {
     // Get current URL with query parameters
     const queryParams = this.route.snapshot.queryParams;
     const url = window.location.href; // Full URL
     this.currentUrl = url;
+  }
+
+  removeQueryParams() {
+    // Store current query params in localStorage
+    // Because Google SignIn requires exact redirect URL
+    const currentQueryParams = this.route.snapshot.queryParams;
+    localStorage.setItem('queryParams', JSON.stringify(currentQueryParams));
+    this.router.navigate([], {
+      replaceUrl: true,
+    });    
+  }
+
+  restoreQueryParams() {
+    const queryParams = localStorage.getItem('queryParams');
+    if (queryParams) {
+      const parsedQueryParams = JSON.parse(queryParams);
+      this.router.navigate([], {
+        queryParams: parsedQueryParams,
+        replaceUrl: true,
+      });
+    }
+  }
+
+  async login() {
+    this.removeQueryParams();
+
+    const response = await SocialLogin.login({
+      provider: 'google',
+      options: {
+        scopes: ['email', 'profile'],
+      },
+    });
+
+    this.restoreQueryParams();
+
+    if (response.provider === 'google') {
+      const result = response.result as GoogleLoginResponseOnline;
+      localStorage.setItem('authToken', result?.idToken ?? ''); // Store the token
+      localStorage.setItem('authResult', JSON.stringify(result)); // Store the full result
+      console.log('Authentication token and result stored in localStorage.');
+    }
+    this.loadSessionDetails();  
+  }
+
+  async logout() {
+    await SocialLogin.logout({ provider: 'apple' });
+    localStorage.removeItem('authToken');
+    localStorage.removeItem('authResult');
+
+    this.loadSessionDetails();
   }
 
   getGymInfo() {
@@ -162,7 +249,7 @@ export class MembershipPage implements OnInit {
     const ORDER_ID = this.generateRandomString();
     const AMOUNT = '1.00'; // Change as required
     const CURRENCY = 'INR';
-    const mc = '7997'; // Gym & Fitness Category
+    const mc = '7399'; // Gym & Fitness Category
 
     // get the current url
     const currentUrl = window.location.href;
